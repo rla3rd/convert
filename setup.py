88a050f9f10315c2ea94d83a98234d124bb8ef5b
Rick Albright <rla3rd@gmail.com>
2016-05-29 09:04:32 -0400

    xml2dict should go here too

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup( name='convert',
     description='automatic string conversion library',
     author='Richard Albright',
     version=__version__,
-    py_modules=['convert', 'dateAdapter'],
+    py_modules=['convert', 'dateAdapter', 'xml2dict'],
     license='MIT License' )
